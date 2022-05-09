c19d6322fc44fd08a5b55fe55e4ea6a6cbb3860b
Joshua Navarro <joshua.navarro35@gmail.com>
2022-05-08 21:17:20 -0500

    [UPDATE] enviroment variables to react app

--- frontend/src/apis/backend.api.js
+++ frontend/src/apis/backend.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseUrl: process.env.REACT_APP_BACKEND_API_BASE_URL
+  baseURL: process.env.REACT_APP_BACKEND_API_BASE_URL
 });
 
 async function createInvitation(email, fullName) {
